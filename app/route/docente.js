b2d2302ac6bb8d95b6fd1d26f0c535310192028c
Mauricio Boaventura <mba097@gmail.com>
2020-03-09 13:59:24 -0300

    Funcao alterar usuario criado

--- app/route/docente.js
+++ app/route/docente.js
@@ -11,6 +11,7 @@ router.get('/', (req, res) => {
     res.send("docente")
 })
 
+//Cadastra novo diretor
 router.post('/cadastrar/direcao', async (req, res) => {
     const {
         nome,
@@ -39,6 +40,8 @@ router.post('/cadastrar/direcao', async (req, res) => {
 
     // res.status(200).send(req.body)
 })
+
+//Cadastra novo docente
 router.post('/cadastrar/docente', async (req, res) => {
     const {
         nome,
@@ -67,6 +70,8 @@ router.post('/cadastrar/docente', async (req, res) => {
 
     // res.status(200).send(req.body)
 })
+
+//Cadastra novo aluno
 router.post('/cadastrar/aluno', async (req, res) => {
     const {
         nome,
@@ -95,5 +100,136 @@ router.post('/cadastrar/aluno', async (req, res) => {
 
     // res.status(200).send(req.body)
 })
+
+//Altera dados do usuario diretor
+router.post('/alterar', async (req, res) => {
+    const {
+        id,
+        nome,
+        cpf,
+        nascimento,
+        email,
+        senhaAntiga,
+        senhaNova
+    } = req.body;
+
+    console.log(nome)
+    console.log(cpf)
+    console.log(nascimento)
+    console.log(email)
+    console.log(senhaAntiga)
+    console.log(senhaNova)
+
+    req.body.nascimento = moment(nascimento, "DD/MM/YYYY")
+
+    //Pesquisa o usuario existente
+    const usuario = await User.findOne({
+        where: {
+            id: id
+        }
+    })
+
+    //Verifica as senhas
+    if (usuario.senha == req.body.senhaAntiga) {
+        console.log("As senhas conferem")
+
+        //Verifica email
+        const usuarioEmail = await User.findOne({
+            where: {
+                email: email
+            }
+        })
+
+        //Verifica se o email esta sendo usado
+        if (usuarioEmail === null) {
+            console.log('Email não cadastrado, pode prosseguir com a atualizacao');
+            let objAtualizado = {
+                nome: nome,
+                cpf: cpf,
+                nascimento: req.body.nascimento,
+                email: email,
+                senha: senhaNova
+            }
+            await User.update(objAtualizado, {
+                where: {
+                    id: id
+                }
+            });
+            res.status(200).send('Objeto atualizado')
+        } else {
+            res.status(400).send('Email já usado')
+        }
+    } else {
+        console.log("Senha errada")
+        res.status(400).send('Senha errada')
+    }
+
+})
+
+//Altera dados do usuario
+router.post('/alterar/aluno', async (req, res) => {
+    const {
+        id,
+        nome,
+        cpf,
+        nascimento,
+        email,
+        senhaAntiga,
+        senhaNova
+    } = req.body;
+
+    console.log(nome)
+    console.log(cpf)
+    console.log(nascimento)
+    console.log(email)
+    console.log(senhaAntiga)
+    console.log(senhaNova)
+
+    req.body.nascimento = moment(nascimento, "DD/MM/YYYY")
+
+    //Pesquisa o usuario existente
+    const usuario = await User.findOne({
+        where: {
+            id: id
+        }
+    })
+
+    //Verifica as senhas
+    if (usuario.senha == req.body.senhaAntiga) {
+        console.log("As senhas conferem")
+
+        //Verifica email
+        const usuarioEmail = await User.findOne({
+            where: {
+                email: email
+            }
+        })
+
+        //Verifica se o email esta sendo usado
+        if (usuarioEmail === null) {
+            console.log('Email não cadastrado, pode prosseguir com a atualizacao');
+            let objAtualizado = {
+                nome: nome,
+                cpf: cpf,
+                nascimento: req.body.nascimento,
+                email: email,
+                senha: senhaNova
+            }
+            await User.update(objAtualizado, {
+                where: {
+                    id: id
+                }
+            });
+        } else {
+            res.status(400).send('Email já usado')
+        }
+    } else {
+        console.log("Senha errada")
+        res.status(400).send('Senha errada')
+    }
+
+})
+
+//Exclui
 
 module.exports = router;
